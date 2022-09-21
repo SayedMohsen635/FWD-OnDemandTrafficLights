36f278bca4f85c65396a6b124439dce9627054b2
Sayed <sayedmohsen635@gmail.com>
2022-09-21 15:12:22 +0200

    Update DIO.h

--- Atmel Project/On-demand_Traffic_Lights/On-demand_Traffic_Lights/MCAL/DIO Driver/DIO.h
+++ Atmel Project/On-demand_Traffic_Lights/On-demand_Traffic_Lights/MCAL/DIO Driver/DIO.h
@@ -15,19 +15,19 @@ typedef unsigned char uint8_t;
 
 /*
 	Function Name        : DIO_vSetPinDir
-	Function Returns     : void
+	Function Returns     : uint8_t
 	Function Arguments   : uint8_t , uint8_t , uint8_t 
 	Function Description : Setting the direction of the pin in a particular register
 */
-void DIO_vSetPinDir(uint8_t portName , uint8_t pinNumber , uint8_t Dir);
+uint8_t DIO_vSetPinDir(uint8_t portName , uint8_t pinNumber , uint8_t Dir);
 
 /*
 	Function Name        : DIO_writePin
-	Function Returns     : void
+	Function Returns     : uint8_t
 	Function Arguments   : uint8_t , uint8_t , uint8_t
 	Function Description : Writing a value to a pin in a particular register
 */
-void DIO_writePin(uint8_t portName , uint8_t pinNumber , uint8_t value);
+uint8_t DIO_writePin(uint8_t portName , uint8_t pinNumber , uint8_t value);
 
 /*
 	Function Name        : DIO_u8readPin
@@ -39,27 +39,27 @@ uint8_t DIO_u8readPin(uint8_t portName , uint8_t pinNumber);
 
 /*
 	Function Name        : DIO_togPin
-	Function Returns     : void
+	Function Returns     : uint8_t
 	Function Arguments   : uint8_t , uint8_t
 	Function Description : Toggling a pin in a particular register
 */
-void DIO_togPin(uint8_t portName , uint8_t pinNumber);
+uint8_t DIO_togPin(uint8_t portName , uint8_t pinNumber);
 
 /*
 	Function Name        : DIO_setPortDir
-	Function Returns     : void
+	Function Returns     : uint8_t
 	Function Arguments   : uint8_t , uint8_t
 	Function Description : Setting the direction of a whole port register
 */
-void DIO_setPortDir(uint8_t portName , uint8_t Dir);
+uint8_t DIO_setPortDir(uint8_t portName , uint8_t Dir);
 
 /*
 	Function Name        : DIO_writePort
-	Function Returns     : void
+	Function Returns     : uint8_t
 	Function Arguments   : uint8_t , uint8_t
 	Function Description : Writing a value to a whole port register
 */
-void DIO_writePort(uint8_t portName , uint8_t value);
+uint8_t DIO_writePort(uint8_t portName , uint8_t value);
 
 /*
 	Function Name        : DIO_readPort
@@ -71,26 +71,26 @@ uint8_t DIO_readPort(uint8_t portName);
 
 /*
 	Function Name        : DIO_vPullUp
-	Function Returns     : void
+	Function Returns     : uint8_t
 	Function Arguments   : uint8_t , uint8_t , uint8_t
 	Function Description : Connecting the internal pull up resistor
 */
-void DIO_vPullUp(uint8_t portName , uint8_t pinNumber , uint8_t connectValue);
+uint8_t DIO_vPullUp(uint8_t portName , uint8_t pinNumber , uint8_t connectValue);
 
 /*
 	Function Name        : DIO_writeLowNibble
-	Function Returns     : void
+	Function Returns     : uint8_t
 	Function Arguments   : uint8_t , uint8_t
 	Function Description : Writing the four low pins of the atmega32 of a specific port register
 */
-void DIO_writeLowNibble(uint8_t portName , uint8_t value);
+uint8_t DIO_writeLowNibble(uint8_t portName , uint8_t value);
 
 /*
 	Function Name        : DIO_writeHighNibble
-	Function Returns     : void
+	Function Returns     : uint8_t
 	Function Arguments   : uint8_t , uint8_t
 	Function Description : Write the four high pins of the atmega32 of a specific port register
 */
-void DIO_writeHighNibble(uint8_t portName , uint8_t value);
+uint8_t DIO_writeHighNibble(uint8_t portName , uint8_t value);
 
 #endif /* DIO_H_ */
